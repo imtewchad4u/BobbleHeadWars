7cc55baf27a6a382d0fa50dd7110a53d7cd55a22
unknown <imtewchad4u@outlook.com>
2019-09-16 15:22:34 -0400

    Commit 3.0

---
+++ Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Destroys the bullet when it goes off screen
+    private void OnBecameInvisible()
+    { Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    //Creates an instance variable to store the character controller
+    private CharacterController characterController;
+    public Rigidbody head;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Gets a reference to the current component passed into the script
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Gets the current position of the game object
-        Vector3 pos = transform.position;
-        //Locks the characters movements to the x and z axis
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        //Updates the game objects position to the new one
-        transform.position = pos;
+        //Creates a new Vector3 to control movement, move direction and move speed
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 
+            0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+    void FixedUpdate()
+    {
+        //Calculate the movement direction
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+            0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {   
+            // TODO
+        }
+        else
+        {
+            //Used to make the head move
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
