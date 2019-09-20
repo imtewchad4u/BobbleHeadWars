20b1976c17eb2e63ac2098f8c1a0b0fa47ea6f73
unknown <imtewchad4u@outlook.com>
2019-09-20 13:16:36 -0400

    Commit 5.1- End of Chapter 5, Needs to fix Collisions

--- Assets/Alien.cs
+++ Assets/Alien.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Alien : MonoBehaviour
 {
+    public Transform target;
+    private NavMeshAgent agent;
+    public float navigationUpdate;
+    private float navigationTime = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position; navigationTime = 0;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
