5577ee0a8900eb39a344c963c6f2f6bac7d4c3e4
unknown <imtewchad4u@outlook.com>
2019-09-23 15:50:29 -0400

    Fixed broken textures and finished chapter 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,13 +32,14 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
-        {   
-            // TODO
+        {
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             //Used to make the head move
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
         //Creates an empty raycast when you get a hit
         RaycastHit hit;
